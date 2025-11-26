6c1d882cbb91069357264476ed5b84e58eecf812
palinaria <polinapuzik7@gmail.com>
2025-11-26 20:53:30 +0300

    add:install sequelize to back

--- backend/migrations/20251126121035-create-article.js
+++ backend/migrations/20251126121035-create-article.js
@@ -1,39 +1,38 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Articles', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      content: {
-        type: Sequelize.TEXT,
-        allowNull: false
-      },
-      files: {
-        type: Sequelize.JSON,
-        allowNull: true,
-        defaultValue: []
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Articles');
-  }
-};
-//
+
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('Articles', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    files: {
+      type: Sequelize.JSON,
+      allowNull: true,
+      defaultValue: []
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  });
+}
+
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('Articles');
+}
