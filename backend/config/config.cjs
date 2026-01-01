e28a2e823a99460f1de1616c2feea665fb174f58
palinaria <polinapuzik7@gmail.com>
2026-01-01 19:36:21 +0300

    fix:add host and port

--- backend/config/config.cjs
+++ backend/config/config.cjs
@@ -5,7 +5,8 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || 'postgres'
   }
 };
